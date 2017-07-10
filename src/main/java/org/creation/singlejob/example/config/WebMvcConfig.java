774558f50c33f73a0db7c4f0e333778aab8f4b0f
199287 <zz1122zz>
2017-07-10 11:49:19 +0800

    初始化提交

---
+++ src/main/java/org/creation/singlejob/example/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package org.creation.singlejob.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
