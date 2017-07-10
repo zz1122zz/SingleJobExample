46f0e53774dbd5fada100435ce10a3b291a8c9c7
199287 <zz1122zz>
2017-07-10 11:49:19 +0800

    初始化提交

---
+++ src/main/java/org/creation/singlejob/example/SingleJobExampleService.java
@@ -0,0 +1,89 @@
+package org.creation.singlejob.example;
+import javax.annotation.Resource;
+
+import org.creation.singlejob.SingleJob;
+import org.creation.singlejob.SingleJobPolicy;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+
+/** 
+* @author 作者 LiuPeng E-mail: 
+* @version 创建时间：2017年7月10日 下午1:41:34 
+* 类说明 
+*/
+/**
+ * <p>Title: Module Information         </p>
+ * <p>Description: Function Description </p>
+ * <p>Copyright: Copyright (c) 2017     </p>
+ * <p>Company: ND Co., Ltd.       </p>
+ * <p>Create Time: 2017年7月10日           </p>
+ * @author LiuPeng
+ * <p>Update Time: 2017年7月10日               </p>
+ * <p>Updater:                          </p>
+ * <p>Update Comments:                  </p>
+ */
+@Service
+public class SingleJobExampleService {
+    @Resource
+    SingleJobExampleService singleJobExampleService;
+
+    /**	
+     * <p>Description:  展示redis锁  </p>
+     * <p>Create Time: 2017年7月11日   </p>
+     * <p>Create author: LiuPeng   </p>
+     * @param uid
+     * @param object
+     * @return
+     */
+    @SingleJob(distinction = "#uid+#object.getString(\"key\")", singleJobPolicy= SingleJobPolicy.WAIT_IN_QUENE_TO_PROCEED)
+    public String lockInRedis(String uid,JSONObject object)
+    {
+        return "cache at:"+new DateTime().toString("MMdd HH:mm:SS");
+    }
+    
+    /**	
+     * <p>Description:      展示redis锁的可重入性        </p>
+     * <p>Create Time: 2017年7月11日   </p>
+     * <p>Create author: LiuPeng   </p>
+     * @param uid
+     * @param object
+     * @param reenter
+     * @return
+     */
+    @SingleJob(distinction = "#uid+#object.getString(\"key\")", singleJobDataPersistenceProvider = "redisSingleJobDataPersistenceProvider",singleJobPolicy= SingleJobPolicy.WAIT_IN_QUENE_TO_PROCEED)
+    public String lockInRedisReenterable(String uid,JSONObject object,int reenter)
+    {
+        if(reenter>0)
+        {
+            //SingleJobExampleService aop = (SingleJobExampleService) AopContext.currentProxy();
+            //return aop.lockInRedisReenterable(uid, object,reenter-1);
+            return singleJobExampleService.lockInRedisReenterable(uid, object,reenter-1);
+        }else
+        {
+            return "cache at:"+new DateTime().toString("MMdd HH:mm:SS");
+        }
+    }
+    
+    /** 
+     * <p>Description:      展示redis锁的可重入性        </p>
+     * <p>Create Time: 2017年7月11日   </p>
+     * <p>Create author: LiuPeng   </p>
+     * @param uid
+     * @param object
+     * @param reenter
+     * @return
+     */
+    @SingleJob(distinction = "#uid+#object.getString(\"key\")",singleJobDataPersistenceProvider = "localMemoryDataPersistenceProvider", singleJobPolicy= SingleJobPolicy.WAIT_IN_QUENE_TO_PROCEED)
+    public String lockInMemoryReenterable(String uid,JSONObject object,int reenter)
+    {
+        if(reenter>0)
+        {
+            return singleJobExampleService.lockInMemoryReenterable(uid, object,reenter-1);
+        }else
+        {
+            return "cache at:"+new DateTime().toString("MMdd HH:mm:SS");
+        }
+    }
+}
---
+++ src/main/java/org/creation/singlejob/example/ctrl/ExampleController.java
@@ -0,0 +1,54 @@
+package org.creation.singlejob.example.ctrl;
+
+import javax.annotation.Resource;
+
+import org.creation.singlejob.example.SingleJobExampleService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+/** 
+* @author 作者 LiuPeng E-mail: 
+* @version 创建时间：2017年7月10日 上午11:23:00 
+* 类说明 
+*/
+/**
+ * <p>Title: Module Information         </p>
+ * <p>Description: Function Description </p>
+ * <p>Copyright: Copyright (c) 2017     </p>
+ * <p>Company: ND Co., Ltd.       </p>
+ * <p>Create Time: 2017年7月10日           </p>
+ * @author LiuPeng
+ * <p>Update Time: 2017年7月10日               </p>
+ * <p>Updater:                          </p>
+ * <p>Update Comments:                  </p>
+ */
+@RestController
+@RequestMapping("/example")
+public class ExampleController {
+
+    @Resource
+    SingleJobExampleService singleJobExampleService;
+    
+    @RequestMapping(value = "/lockInRedis", method = RequestMethod.POST, produces = { "application/json;charset=UTF-8" })
+    @ResponseBody
+    public Object lockInRedis(@RequestBody JSONObject object, String uid) {
+        return singleJobExampleService.lockInRedis(uid, object);
+    }
+
+    @RequestMapping(value = "/lockInRedisReenterable", method = RequestMethod.POST, produces = { "application/json;charset=UTF-8" })
+    @ResponseBody
+    public Object lockInRedisReenterable(@RequestBody JSONObject object, String uid) {
+        return singleJobExampleService.lockInRedisReenterable(uid, object, 3);
+    }
+    
+    @RequestMapping(value = "/lockInMemoryReenterable", method = RequestMethod.POST, produces = { "application/json;charset=UTF-8" })
+    @ResponseBody
+    public Object lockInMemoryReenterable(@RequestBody JSONObject object, String uid) {
+        return singleJobExampleService.lockInMemoryReenterable(uid, object, 3);
+    }
+}
